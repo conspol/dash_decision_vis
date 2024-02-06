b099e845ac911896372e984f3b54054eb3887188
Konstantin Polev <conspol7@gmail.com>
2024-02-06 12:59:04 +0900

    style: type annotation definition

--- dash_decision_vis/type_vars.py
+++ dash_decision_vis/type_vars.py
@@ -1,5 +1,5 @@
-from typing import List, Dict
+from typing import List, Dict, Union
 from .plot_node import PlotNode
 
 
-TPlotInstances = Dict[int, List['TPlotInstances' | PlotNode]]
+TPlotInstances = Dict[int, List[Union['TPlotInstances', PlotNode]]]
